ed6735bc39d129dcf337c9be6d43ca3240b9a551
Matheus <matheusgaldinoinfo@gmail.com>
2022-04-29 15:02:51 -0300

    Implementação da pag Contato

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
+import Contato from './paginas/contato/Contato'
 import './App.css';
 
 function App() {
   return (
   <>
   <Navbar />
+  <Contato />
   <Footer />
+  
   </>
   );
 }
---
+++ src/paginas/contato/Contato.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Contato.css';
+	
+function Contato() {
+    return (
+        <>
+          <div>
+<h1 className='titulo' >Sobre nós</h1>
+<h3 className='center'>Somos um ecommerce, idealizado em 2022, com a intenção de diminuir o descarte de materiais eletrônicos.
+    <p></p>Possibilitamos sua compra/venda.
+</h3>
+</div>
+<div className='pad'>
+<h2 className='titulo'>Entre em contato</h2>
+<h3 className='center'>
+E-mail: sustech03@gmail.com
+<p></p>
+Github: https://github.com/SustechGenT47
+</h3>
+          </div>
+        </>
+    );
+}
+
+export default Contato;
+
