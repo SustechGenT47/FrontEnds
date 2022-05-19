c4b7469171998c0e978754fdff083492c1139846
Gustavo <gustavossantos41@outlook.com>
2022-05-19 14:34:39 -0300

    Página de Login feita

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { Typography, TextField, Button } from '@material-ui/core';
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
 import UserLogin from '../../models/UserLogin';
+
 
 import './Login.css';
 import { login } from '../../services/Service';
 import { addToken } from '../../Store/tokens/Actions';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
+
+import imagemLogin from '../../assets/imagemLogin.gif';
 
 
 function Login() {
@@ -70,40 +73,43 @@ function Login() {
         }
     }
 
-    return (
-    <div className=" App" id="dark">
-<h3>
-    Bem vindo a 
-    </h3>
- 
- 
-<h3> SUStech</h3>
-
-        <form className="form" onSubmit={logar}>
-            <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' variant='outlined' name='usuario' margin='normal' className="username" placeholder="Usuário" fullWidth />
-            <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' variant='outlined' name='senha' margin='normal' className="password" type='password' placeholder="Senha" fullWidth />
-    
-            <Button type='submit' variant='contained' className='submit'> Logar </Button>
-            <p className="forgot">
-                <Link to='/cadastro' className='text-decorator-none'>
-                    <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta? Cadastre-se</Typography>
-                </Link>
-            </p>
-        </form>
-        </div>
-
-    
-
-
-
-
-
+        return (
+            <Grid xs={12} container direction='row' justifyContent='center' alignItems='center' id="darkBackground">
+                        <Grid item xs={6} alignItems='center' className="formBackground" justifyContent = 'center'>
+                                <form className="formLogin" onSubmit={logar}>
+                                    <h3 className='colorText'>Olá! Seja bem vindo.</h3>
+                                        <h5 className='colorText'>Faça o seu login</h5>
+                                    <p></p>
+                                    <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' variant='outlined' name='usuario' margin='normal' className="username" placeholder="Usuário" fullWidth />
+                                    <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' variant='outlined' name='senha' margin='normal' className="password" type='password' placeholder="Senha" fullWidth />
+                            
+                                    <Button type='submit' variant='contained' className="submit botaoEntrar botaoEntrar:hover">Entrar</Button>
+                                    <p className="forgot">
+                                        <Link to='/cadastro' className='text-decorator-none'>
+                                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta? Cadastre-se</Typography>
+                                        </Link>
+                                    </p>
+                                  
+                                </form>
+                            <Box my = {12.3} >
+                                <img src={imagemLogin} className="imagem2"/>
+                            </Box>
+                            
+                        </Grid>    
+                
+            </Grid>
+            
 
 
 
 
-    )
 
-}
+
+
+
+
+        );
+
+    }
 
 export default Login;
