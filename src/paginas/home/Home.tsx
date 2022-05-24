d12a37f456fc9777de74390dbcfb2b31cbee5e71
Gustavo <gustavossantos41@outlook.com>
2022-05-24 10:22:55 -0300

    alterações

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -19,22 +19,10 @@ function Home(){
                     <Box  display = 'flex'justifyContent='center' alignItems='center' >
                       <TabProduto />
                     </Box>
-                </Grid>
-
-                <Grid xs={6} > 
-             <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                            </Box>
-                            
-                
-                        <Link to="/listarProdutos" className='text-decorator-none'>
-                        <Button variant="outlined" className='botaoHome'>Ver Produtos</Button>
-                        </Link>
-                    </Box>
-             </Grid>
+                </Grid>  
             </Grid>
 
-            </div>
+        </div>
     );
 }
 
